f4b6242340ab749c8aa8cb3f514ef747b539c2ab
fidaaysha <masszippyc@gmail.com>
2024-05-13 21:16:24 +0530

    'latest'

--- catloads/settings.py
+++ catloads/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j9_@c%1_sz#i#nqfluyd9h=&fkj%adcad@e5q=4!y#v+qhy71q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
