1dd383b740692bcf096d86c47d7357119172df17
fidaaysha <masszippyc@gmail.com>
2024-06-24 19:26:44 +0530

    'latest'

--- catloads_web/admin.py
+++ catloads_web/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser,CartItem,Cart,OrderItem,Order,Product,ProductImages,ProductVideos,Category
+from .models import CustomUser,CartItem,Cart,OrderItem,Order,Product,ProductImages,ProductVideos,Category,ProductSale,ProductSaleItems
 
 # Register your models here.
 admin.site.register(CustomUser)
